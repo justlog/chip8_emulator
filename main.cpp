41e200c184a42b7cd75a2396fd95aaa7196bb6de
justlog <lmerkelgolan@gmail.com>
2025-10-27 21:47:41 +0200

    added a bunch of instructions, timers, button press

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <cstdint>
+#include <cstdlib>
 #include <fstream>
 #include <vector>
 #include <assert.h>
@@ -21,6 +22,8 @@ typedef uint8_t  u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
+typedef float f32;
+typedef double f64;
 typedef bool b8;
 std::vector<char> LoadROM(const char* path){
     std::ifstream inputFile(path, std::ios::binary);
@@ -112,7 +115,7 @@ struct Chip8Context {
 			u8 VF;
 		};
 		u8 registers[16];
-	} GPRegisters;
+	};
 	union{
 		struct{
 			b8 btn0;
@@ -132,16 +135,40 @@ struct Chip8Context {
 			b8 btnE;
 			b8 btnF;
 		};
-		b8 controls[16];
+		b8 buttons[16];
 	};
+  u32 instructionPerformed = 0;
+  b8 getKey = false;
+  u8 getKeyPressed = 0xFF;
 };
 
+constexpr u32 BYTES_PER_FONT = 5;
+constexpr u8 FONT[] = {
+  0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+  0x20, 0x60, 0x20, 0x20, 0x70, // 1
+  0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+  0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+  0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+  0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+  0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+  0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+  0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+  0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+  0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+  0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+  0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+  0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+  0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+  0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
 void InitChip8Context(Chip8Context* ctx){
 	//Allocate memory for ram
 	ctx->ram = (i8*)malloc(4096);
 	if(!ctx->ram) {
 		MEM_ALLOC_ERR();
 	}
+  for(u32 i = 0; i < sizeof(FONT)/sizeof(FONT[0]); i++)
+    ctx->ram[i] = FONT[i];
 	// ctx->stack = (i16*)malloc(STACK_SIZE);
 	// if(!ctx->stack) {
 	// 	MEM_ALLOC_ERR();
@@ -179,10 +206,35 @@ enum Operation {
   ANDX_REG = 0x8002,
   XORX_REG = 0x8003,
 	ADDX_REG = 0x8004,
-  SUBX_REG = 0x8005,
+  SUB_REG = 0x8005,
+  SUBN_REG = 0x8007,
+  /*NOTE: shifting are ambiguous instructions: 
+  the CHIP-8 interpreter for the original COSMAC VIP, this instruction did the following: It put the value of VY into VX, and then shifted the value in VX 1 bit to the right (8XY6) or left (8XYE). VY was not affected, but the flag register VF would be set to the bit that was shifted out.
+  However, starting with CHIP-48 and SUPER-CHIP in the early 1990s, these instructions were changed so that they shifted VX in place, and ignored the Y completely.
+  
+  This is one of the main differences between implementations that cause problems for programs. Since different games expect different behavior, you could consider making the behavior configurable by the user.*/
+  SHR = 0x8006,
+  SHL = 0x800E,
 	ADDX_IMM = 0x7000,
 	SETI = 0xA000,
-	DRAW = 0xD000
+  /*Ambiguous instruction!
+  In the original COSMAC VIP interpreter, this instruction jumped to the address NNN plus the value in the register V0. This was mainly used for “jump tables”, to quickly be able to jump to different subroutines based on some input.
+  Starting with CHIP-48 and SUPER-CHIP, it was (probably unintentionally) changed to work as BXNN: It will jump to the address XNN, plus the value in the register VX. So the instruction B220 will jump to address 220 plus the value in the register V2.
+  The BNNN instruction was not widely used, so you might be able to just implement the first behavior (if you pick one, that’s definitely the one to go with). If you want to support a wide range of CHIP-8 programs, make this “quirk” configurable.*/
+  JPOFFSET = 0xB000,//TODO: I can try to configure this later on, supposedly the instruction is not widely used.
+  RND = 0xC000,
+  //Skip if de/pressed
+  SKP = 0xE09E,
+  SKNP = 0xE0A1,
+	DRAW = 0xD000,
+  //Timers
+  LDX_TIMER = 0xF007,
+  LD_DT = 0xF015,//Load delay timer
+  LD_ST = 0xF018,//Load sound timer
+  ADDI_X = 0xF01E,//Unlike other arithmetic instructions, this did not affect VF on overflow on the original COSMAC VIP. However, it seems that some interpreters set VF to 1 if I “overflows” from 0FFF to above 1000 (outside the normal addressing range). This wasn’t the case on the original COSMAC VIP, at least, but apparently the CHIP-8 interpreter for Amiga behaved this way. At least one known game, Spacefight 2091!, relies on this behavior. I don’t know of any games that rely on this not happening, so perhaps it’s safe to do it like the Amiga interpreter did.
+  LD_KEY = 0xF00A,
+  LD_FONT = 0xF029,//Load I register with the address of font for character in X
+  BCD = 0xF033,
 };
 
 // std::unordered_map<Operation, std::string> OperationToString = {
@@ -252,8 +304,8 @@ std::string OperationToString(u16 inst){
 void DrawDisplay(SDL_Renderer* renderer, Chip8Context& ctx, u8 X, u8 Y, u8 N)
 {
   std::cout << "Draw display function" << std::endl;
-  u8 x = ctx.GPRegisters.registers[X] % CHIP8_DISPLAY_WIDTH;
-  u8 y = ctx.GPRegisters.registers[Y] % CHIP8_DISPLAY_HEIGHT;
+  u8 x = ctx.registers[X] % CHIP8_DISPLAY_WIDTH;
+  u8 y = ctx.registers[Y] % CHIP8_DISPLAY_HEIGHT;
   u8 countBytes = N;
   for(u8 row = 0; row < countBytes; row++){
     u8 pixelByte = ctx.ram[ctx.indexRegister+row];
@@ -280,6 +332,7 @@ void DrawDisplay(SDL_Renderer* renderer, Chip8Context& ctx, u8 X, u8 Y, u8 N)
       SDL_RenderDrawPoint(renderer, column, row);
     }
   }
+  SDL_RenderPresent(renderer);
 }
 int main(int argc, char* argv[]) {
 	// Initialize SDL
@@ -336,20 +389,79 @@ int main(int argc, char* argv[]) {
 
 	
   u32 cycle = 1;
-  u8 tick = true;
+  b8 emulate = true;
+  b8 timerTick = true;
+  std::unordered_map<SDL_Scancode, u8> buttonMap = {
+    {SDL_SCANCODE_0, 0x0},
+    {SDL_SCANCODE_1, 0x1},
+    {SDL_SCANCODE_2, 0x2},
+    {SDL_SCANCODE_3, 0x3},
+    {SDL_SCANCODE_4, 0x4},
+    {SDL_SCANCODE_5, 0x5},
+    {SDL_SCANCODE_6, 0x6},
+    {SDL_SCANCODE_7, 0x7},
+    {SDL_SCANCODE_8, 0x8},
+    {SDL_SCANCODE_9, 0x9},
+    {SDL_SCANCODE_A, 0xA},
+    {SDL_SCANCODE_B, 0xB},
+    {SDL_SCANCODE_C, 0xC},
+    {SDL_SCANCODE_D, 0xD},
+    {SDL_SCANCODE_E, 0xE},
+    {SDL_SCANCODE_F, 0xF},
+  };
+  u64 counterFrequency = SDL_GetPerformanceFrequency();
+  u64 lastFrame = SDL_GetPerformanceCounter();
+  f64 lastTimerTick = 0.0;
+  f64 secondTimer = 0.0;
+  constexpr f64 msPerTimerTick = (1.0/60.0) * 1000.0;
+  constexpr u32 INST_PER_SECOND = 700;//TODO: https://tobiasvl.github.io/blog/write-a-chip-8-emulator/ claims the speed should be reconfigurable because of different processors it ran on. but it also says a "standard" speed of 700 instructions per second fits well enough for chip8 programs.
 	// Main loop
 	while (!quit) {
+    //Timer ticks
+    //TODO: currently busy waiting, switch to non-busy?
+    u64 currentFrame = SDL_GetPerformanceCounter();
+    f64 dt = ((currentFrame - lastFrame) / (f64)counterFrequency) * 1000;//in ms
+    lastTimerTick += dt;
+    secondTimer += dt;
+    lastFrame = currentFrame;
+    if(lastTimerTick >= msPerTimerTick){
+      if(ctx.delayTimer > 0){
+        ctx.delayTimer -= 1;
+      }
+      if(ctx.soundTimer > 0){
+        ctx.soundTimer -= 1;
+      }
+      lastTimerTick -= msPerTimerTick;
+    }
+    if(secondTimer >= 1000.0){
+      if(emulate){
+        std::cout << "EMULATION TOO SLOW, ONLY " << ctx.instructionPerformed << " WERE PREFORMED" << std::endl;
+      }
+      secondTimer -= 1000.0;
+      emulate = true;
+    }
+
 		// Handle events
 		while (SDL_PollEvent(&e) != 0) {
 			if (e.type == SDL_QUIT)
 				quit = true;
+      if(e.type == SDL_KEYDOWN){
+        if(auto it = buttonMap.find(e.key.keysym.scancode); it != buttonMap.end()){
+          std::cout << "chip8 key " << SDL_GetKeyName(SDL_GetKeyFromScancode(e.key.keysym.scancode)) << " pressed" << std::endl;
+          ctx.buttons[it->second] = true;
+          if(ctx.getKey){
+            ctx.getKeyPressed = it->second;//On the original COSMAC VIP, the key was only registered when it was pressed and then released.
+          }
+        }
+      }
 			if (e.type == SDL_KEYUP){//TODO Controls
-        if(e.key.keysym.sym == SDLK_SPACE){
-          tick = true;
+        if(auto it = buttonMap.find(e.key.keysym.scancode); it != buttonMap.end()){
+          std::cout << "chip8 key " << SDL_GetKeyName(SDL_GetKeyFromScancode(e.key.keysym.scancode)) << " released" << std::endl;
+          ctx.buttons[it->second] = false;
         }
 			}
 		}
-    if(tick){
+    if(emulate){
       u8 byte1 = ctx.ram[ctx.PC++];
       u8 byte2 = ctx.ram[ctx.PC++];
       u16 inst = (((u16)byte1) << 8) | ((u16)byte2);
@@ -362,7 +474,7 @@ int main(int argc, char* argv[]) {
       u16 NNN = (u16)MASK_NNN(inst);
 
       u16 maskedInst = inst & OP_MASK;//NOTE: This is a bad idea, you're masking in two different ways... once in MASK_OP and once like this.
-      std::cout << "cycle " << cycle++ << " performing " << OperationToString(inst) << std::endl;
+      // std::cout << "cycle " << cycle++ << " performing " << OperationToString(inst) << std::endl;
       switch(maskedInst){
         case 0:
           switch(inst){
@@ -376,27 +488,85 @@ int main(int argc, char* argv[]) {
               break;
           }
           break;
+        case 0xE:
+          switch(inst & 0xE0FF){//Need to mask off X to get the opcode 
+            case Operation::SKP:
+              if(ctx.registers[X] <= 0xF && ctx.buttons[ctx.registers[X]]){
+                ctx.PC += 2;
+              }
+              break;
+            case Operation::SKNP:
+              if(ctx.registers[X] <= 0xF && !ctx.buttons[ctx.registers[X]]){
+                ctx.PC += 2;
+              }
+              break;
+          }
+          break;
+        case 0xF:
+          switch(inst & 0xF0FF){
+            case LDX_TIMER:
+              ctx.registers[X] = ctx.delayTimer;
+              break;
+            case LD_DT:
+              ctx.delayTimer = ctx.registers[X];
+              break;
+            case LD_ST:
+              ctx.soundTimer = ctx.registers[X];
+              break;
+            case ADDI_X:
+               //NOTE:  Unlike other arithmetic instructions, this did not affect VF on overflow on the original COSMAC VIP. However, it seems that some interpreters set VF to 1 if I “overflows” from 0FFF to above 1000 (outside the normal addressing range). This wasn’t the case on the original COSMAC VIP, at least, but apparently the CHIP-8 interpreter for Amiga behaved this way. At least one known game, Spacefight 2091!, relies on this behavior. I don’t know of any games that rely on this not happening, so perhaps it’s safe to do it like the Amiga interpreter did.
+              if(ctx.indexRegister + ctx.registers[X] > 0x1000){
+                ctx.VF = 1;
+              }
+              ctx.indexRegister += ctx.registers[X];
+              break;
+            case Operation::LD_KEY:
+              if(ctx.getKey && ctx.getKeyPressed <= 0xF){
+                ctx.registers[X] = ctx.getKeyPressed;
+                ctx.getKeyPressed = 0xFF; //Set back to invalid value.
+                ctx.getKey = false;
+              }
+              else{
+                ctx.getKey = true;
+                ctx.PC -= 2;
+              }
+              break;
+            case Operation::LD_FONT:
+              ctx.indexRegister = (ctx.registers[X] & 0xF)*BYTES_PER_FONT;
+              break;
+            case Operation::BCD:
+              {
+                u8 b = ctx.registers[X];
+                u8 i = 0;
+                while(b > 0){
+                  ctx.ram[ctx.indexRegister + i++] = b%10;
+                  b /= 10;
+                }
+              }
+              break;
+          }
+          break;
         case Operation::CALL:
           ctx.stack.Push(ctx.PC);
           ctx.PC = NNN;
           break;
         case Operation::SE_IMM:
-          if(ctx.GPRegisters.registers[X] == NN){
+          if(ctx.registers[X] == NN){
             ctx.PC += 2;
           }
           break;
         case Operation::SNE_IMM:
-          if(ctx.GPRegisters.registers[X] != NN){
+          if(ctx.registers[X] != NN){
             ctx.PC += 2;
           }
           break;
         case Operation::SE_REG:
-          if(ctx.GPRegisters.registers[X] == ctx.GPRegisters.registers[Y]){
+          if(ctx.registers[X] == ctx.registers[Y]){
             ctx.PC += 2;
           }
           break;
         case Operation::SNE_REG:
-          if(ctx.GPRegisters.registers[X] != ctx.GPRegisters.registers[Y]){
+          if(ctx.registers[X] != ctx.registers[Y]){
             ctx.PC += 2;
           }
           break;
@@ -404,59 +574,75 @@ int main(int argc, char* argv[]) {
           ctx.PC = NNN;
           break;
         case Operation::DRAW:
-          {
-            DrawDisplay(renderer, ctx, X, Y, N);
-          }
+          DrawDisplay(renderer, ctx, X, Y, N);
           break;
         //---- 0x8000 instructions, need to mask last nibble aswell
         case 0x8000:
           switch(inst & 0x800f){
             case Operation::LDX_REG:
-              ctx.GPRegisters.registers[X] = ctx.GPRegisters.registers[Y];
+              ctx.registers[X] = ctx.registers[Y];
               break;
             case Operation::ORX_REG:
-              ctx.GPRegisters.registers[X] |= ctx.GPRegisters.registers[Y];
+              ctx.registers[X] |= ctx.registers[Y];
               break;
             case Operation::ANDX_REG:
-              ctx.GPRegisters.registers[X] &= ctx.GPRegisters.registers[Y];
+              ctx.registers[X] &= ctx.registers[Y];
               break;
             case Operation::XORX_REG:
-              ctx.GPRegisters.registers[X] ^= ctx.GPRegisters.registers[Y];
+              ctx.registers[X] ^= ctx.registers[Y];
               break;
             case Operation::ADDX_REG:
               {
-                u16 sum = (u16)ctx.GPRegisters.registers[X] + (u16)ctx.GPRegisters.registers[Y];
-                ctx.GPRegisters.VF = sum > 255 ? 1 : 0;
-                ctx.GPRegisters.registers[X] = (u8)sum;
+                u16 sum = (u16)ctx.registers[X] + (u16)ctx.registers[Y];
+                ctx.VF = sum > 255 ? 1 : 0;
+                ctx.registers[X] = (u8)sum;
               }
               break;
-            case Operation::SUBX_REG:
-              ctx.GPRegisters.VF =  ctx.GPRegisters.registers[X] > ctx.GPRegisters.registers[Y] ? 1 : 0;
-              ctx.GPRegisters.registers[X] -= ctx.GPRegisters.registers[Y];
+            case Operation::SUB_REG:
+              ctx.VF =  ctx.registers[X] > ctx.registers[Y] ? 1 : 0;
+              ctx.registers[X] -= ctx.registers[Y];
+              break;
+            case Operation::SUBN_REG:
+              ctx.VF =  ctx.registers[X] < ctx.registers[Y] ? 1 : 0;
+              ctx.registers[X] = ctx.registers[Y] - ctx.registers[X];
+              break;
+            //NOTE shifting are ambiguous instructions, might want to have configurable behaviour, see enum defintion.
+            case Operation::SHR:
+              ctx.VF = ctx.registers[X] & 0x1 ? 1 : 0;//Check if shifted bit is 1.
+              ctx.registers[X] >>= 1;
+              break;
+            case Operation::SHL:
+              ctx.VF = ctx.registers[X] & 0x80 ? 1 : 0;//Check if shifted bit is 1.
+              ctx.registers[X] <<= 1;
               break;
           }
           break;
         case Operation::LDX_IMM:
-          ctx.GPRegisters.registers[X] = NN;
+          ctx.registers[X] = NN;
           break;
         case Operation::ADDX_IMM:
-          ctx.GPRegisters.registers[X] += NN;
+          ctx.registers[X] += NN;
           break;
         case Operation::SETI:
           ctx.indexRegister = NNN;
           break;
+        case Operation::RND:
+          {
+            u8 rand = (u8)(std::rand() % 255);
+            ctx.registers[X] = NN & rand;//TODO: http://devernay.free.fr/hacks/chip8/C8TECH10.HTM points to the instruction AND of the chip8. but https://tobiasvl.github.io/blog/write-a-chip-8-emulator/ doesn't mention it. I wonder if the behaviour is supposed to match the regular instruction (i.e. set the VF register)
+            break;
+          }
         default:
           std::cout << "Could not preform instruction " << std::hex << inst; 
           break;
       }
+      ctx.instructionPerformed++;
 
-      // Clear screen with black
-      // SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-      // SDL_RenderClear(renderer);
 
-      // Update screen
-      SDL_RenderPresent(renderer);
-      // tick = false;
+      if(ctx.instructionPerformed >= INST_PER_SECOND){
+        ctx.instructionPerformed = 0;
+        emulate = false;
+      }
     }
 	}
 
